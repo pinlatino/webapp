96703ccf961194fbd153c4d084e8e8715b82d17b
Gizem Ocak <carloseriveros@gmail.com>
2019-11-12 14:50:45 -0500

    adding db env for production

--- knexfile.js
+++ knexfile.js
@@ -31,13 +31,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOSTNAME,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    },
+    connectionString: process.env.DATABASE_URL,
     migrations: {
       directory: "./db/migrations"
     },
