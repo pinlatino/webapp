6cbf98c749737df92e71cc104629771041d14b8a
Gizem Ocak <carloseriveros@gmail.com>
2019-11-13 13:00:37 -0500

    adding console log for url

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,8 @@
 const environment = process.env.NODE_ENV || "development";
 const config = require("../knexfile")[environment];
-console.log("process.env.DATABASE_URL knex", process.env.DATABASE_URL);
+console.log(
+  "process.env.DATABASE_URL knex",
+  environment,
+  process.env.DATABASE_URL
+);
 module.exports = require("knex")(config);
